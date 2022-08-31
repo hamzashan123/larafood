5d6f494e293e56e6408d9c5ca3f6a3c9fb8db483
ameen-ulhaq <dev.ameenulhaq@gmail.com>
2022-08-31 08:54:17 +0500

    c

--- resources/js/views/MealDetail.vue
+++ resources/js/views/MealDetail.vue
@@ -56,7 +56,7 @@
 
                     <div class="shop-tabs">
 
-                        <ul class="nav nav-pills" id="pills-tab" role="tablist">
+                        <ul class="nav nav-pills d-flex" id="pills-tab" role="tablist">
                             <li class="nav-item" role="presentation">
                                 <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true"> <h6 class="nav-link-title">Daily Meals</h6> </button>
                             </li>
